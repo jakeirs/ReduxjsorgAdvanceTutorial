da0239325410ecd4080f3cee21ee30c9c0a003f7
Szymon <kierej@gmail.com>
2017-10-17 23:11:05 +0200

    added components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import 'babel-polyfill'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import { render } from 'react-dom'
+import Root from './containers/Roots'
+
+render(
+  <Root />,
+  document.getElementById('root')
+)
